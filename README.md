1006edc52482f9c8bd57c5409d4138c82917c699
Harshit shukla <106909875+harsshht@users.noreply.github.com>
2023-03-03 18:16:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
+
 # ChatHere
+
+This is a simple chat app that allows users to communicate with each other in real-time. It uses a client-server architecture where multiple clients can connect to a central server to send and receive messages.
+
+
+Live Link : https://chathereapp.netlify.app/login
+
+## Screenshots
+
+
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
---
+++ src/App.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+import "./style.scss";
+// import "./style.css";
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+function App() {
+  const { currentUser } = useContext(AuthContext);
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
